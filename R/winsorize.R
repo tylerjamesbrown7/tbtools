8a4914569e2560cce2e8b040b6d1fcba157be7ac
Tyler Brown <tylerjamesbrown7@gmail.com>
2023-09-18 00:48:37 +0200

    fixes

--- R/winsorize.R
+++ R/winsorize.R
@@ -1,7 +1,7 @@
 #' Winsorizes outliers based on a percentile
 #'
 #' @export
-winsorize <- function(list, percentile = 0.90, bounds = c(min(list[!is.na(list)]), max(list[!is.na(list)]))){
+winsorize <- function(list, percentile = 0.90, bounds = c(min(list[!is.na(list) & is.finite(list)]), max(list[!is.na(list) & is.finite(list)]))){
   if (missing(bounds) & (percentile < 1 & percentile > 0)){
     windsor_bounds <- stats::quantile(list, probs = c((1-percentile)/2, percentile + (1-percentile)/2), na.rm=T)
     list[list < windsor_bounds[1]] <- windsor_bounds[1]
@@ -14,7 +14,7 @@ winsorize <- function(list, percentile = 0.90, bounds = c(min(list[!is.na(list)]
     list[list > bounds[2]] <- bounds[2]
 
     return(list)
-  } else if (bounds[1] < min(list[!is.na(list)]) | bounds[2] > max(list[!is.na(list)])) {
+  } else if (bounds[1] < min(list[!is.na(list) & is.finite(list)]) | bounds[2] > max(list[!is.na(list) & is.finite(list)])) {
     stop("Bounds outside of data range")
   } else if (percentile > 1 | percentile < 0) {
     stop("Percentile must be btx. 0 and 1")

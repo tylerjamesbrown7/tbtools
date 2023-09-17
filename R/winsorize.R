89b68b5f8451cd29d28564acabf69f492eb7df66
Tyler Brown <tylerjamesbrown7@gmail.com>
2023-09-18 00:05:56 +0200

    fies

--- R/winsorize.R
+++ R/winsorize.R
@@ -1,7 +1,7 @@
 #' Winsorizes outliers based on a percentile
 #'
 #' @export
-winsorize <- function(list, percentile = 0.90, bounds = c(min(list), max(list))){
+winsorize <- function(list, percentile = 0.90, bounds){
   if (missing(bounds) & (percentile < 1 & percentile > 0)){
     windsor_bounds <- stats::quantile(list, probs = c((1-percentile)/2, percentile + (1-percentile)/2), na.rm=T)
     list[list < windsor_bounds[1]] <- windsor_bounds[1]

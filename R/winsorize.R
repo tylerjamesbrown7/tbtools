32774743016a571675431eac31f8e8f9b325b89c
Tyler Brown <tylerjamesbrown7@gmail.com>
2023-09-17 21:03:46 +0200

    updated plots

--- R/winsorize.R
+++ R/winsorize.R
@@ -4,7 +4,7 @@
 winsorize <- function(list, percentile = 0.90){
 
   if (percentile < 1 & percentile > 0){
-    bounds <- quantile(list, probs = c((1-percentile)/2, percentile + (1-percentile)/2), na.rm=T)
+    bounds <- stats::quantile(list, probs = c((1-percentile)/2, percentile + (1-percentile)/2), na.rm=T)
     list[list < bounds[1]] <- bounds[1]
     list[list > bounds[2]] <- bounds[2]
     return(list)

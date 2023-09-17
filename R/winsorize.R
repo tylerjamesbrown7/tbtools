5bdf0c084f90b5b121ebccdcb1bb20f80d9859c0
Tyler Brown <tylerjamesbrown7@gmail.com>
2023-09-18 00:07:06 +0200

    fies

--- R/winsorize.R
+++ R/winsorize.R
@@ -1,13 +1,13 @@
 #' Winsorizes outliers based on a percentile
 #'
 #' @export
-winsorize <- function(list, percentile = 0.90, bounds){
+winsorize <- function(list, percentile = 0.90, bounds = c(min(list), max(list))){
   if (missing(bounds) & (percentile < 1 & percentile > 0)){
     windsor_bounds <- stats::quantile(list, probs = c((1-percentile)/2, percentile + (1-percentile)/2), na.rm=T)
     list[list < windsor_bounds[1]] <- windsor_bounds[1]
     list[list > windsor_bounds[2]] <- windsor_bounds[2]
     return(list)
-  } else if (!missing(bounds) & (bounds[1] > min(list) & bounds[2] < max(list) )) {
+  } else if ((bounds[1] > min(list) & bounds[2] < max(list) )) {
     list[list < bounds[1]] <- bounds[1]
     list[list > bounds[2]] <- bounds[2]
     return(list)
